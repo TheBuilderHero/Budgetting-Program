63fc2c72c564ec1f784df356ac413d8d8b04de12
Dakota Stephens <kota.helives@gmail.com>
2025-01-28 19:22:58 -0600

    Menu open and save functions work

--- Program.py
+++ Program.py
@@ -1,15 +1,20 @@
 # make sure to pip3 install openpyxl
 import openpyxl
-# GUI:
+# File GUI:
 import tkinter as tk
 import tkinter.filedialog as filedialog
 from tkinter import ttk
+from tkinter.filedialog import asksaveasfile 
 # CSV to Excel formatting:
 import csv
 import os # file extension removal
 # Menu bar:
 from tkinter import Menu
 from tkinter import OptionMenu
+#TreeView data to Excel:
+import pandas as pd
+
+excel_save_file_extension = '.xlsx'
 
 
 
@@ -19,26 +24,6 @@ r = tk.Tk()
 r.resizable(width=False, height=False)
 r.title('Budgetting App')
 
-#Adding a menu Bar:
-
-def donothing():
-    return
-
-menubar = Menu(r)
-filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(label="New", command=donothing)
-filemenu.add_command(label="Open", command=donothing)
-filemenu.add_command(label="Save", command=donothing)
-filemenu.add_separator()
-filemenu.add_command(label="Exit", command=r.quit)
-menubar.add_cascade(label="File", menu=filemenu)
-
-helpmenu = Menu(menubar, tearoff=0)
-helpmenu.add_command(label="Help Index", command=donothing)
-helpmenu.add_command(label="About...", command=donothing)
-menubar.add_cascade(label="Help", menu=helpmenu)
-
-r.config(menu=menubar)
 
 # Using treeview widget    
 treev = ttk.Treeview(r, selectmode ='browse')
@@ -81,7 +66,7 @@ def show_object(ob, appen, fill):
     for ite in ob:
         ite.pack(expand = True, fill=fill, side=appen)
 def open_file():
-    file_path = filedialog.askopenfilename(filetypes=[('Excel', '*.xlsx'),('Excel', '*.xlsm'),('Excel', '*.xlsb'),('Excel', '*.xltx'), ('CSV files', '*.csv')])
+    file_path = filedialog.askopenfilename(filetypes=[('Excel', '*.xlsx'),('Excel', '*.xlsm'),('Excel', '*.xlsb'),('Excel', '*.xltx'),('Excel','*.xls'), ('CSV files', '*.csv')])
     if file_path:
         print("Selected file:", file_path)
         return file_path
@@ -99,7 +84,7 @@ def convert_csv_to_excel(fileName): #returns the new file name
 
     filename_without_extension = os.path.splitext(fileName)[0]
 
-    newFileName = filename_without_extension + '.xlsx'
+    newFileName = filename_without_extension + excel_save_file_extension
     wb.save(newFileName)
 
     return newFileName
@@ -119,13 +104,13 @@ def convert_csv_to_excel(fileName): #returns the new file name
 
 def load_excel_file():
     file_path = open_file()
-    file_extension = os.path.splitext(file_path)[1]
-    print("Extension: ", file_extension)
-    print("Path: ", file_path)
-    if file_extension == '.CSV':
-        file_path = convert_csv_to_excel(file_path)
-        print("File has been converted from ", file_extension, " to ", os.path.splitext(file_path)[1])
     if file_path:
+        
+        #check if files are CSV then convert to excel:
+        file_extension = os.path.splitext(file_path)[1]
+        if file_extension == '.CSV':
+            file_path = convert_csv_to_excel(file_path)
+        
         # Load the workbook
         wb = openpyxl.load_workbook(file_path)
 
@@ -154,13 +139,56 @@ def load_excel_file():
         # Defining heading
         treev['show'] = 'headings'
 
-#this runs the program to prompt for a file.
-load_excel_file()
-#button = tk.Button(r, text='Open File', width=25, command=button_click)
-#show_object(button,"bottom","none")
+
+#Tree Saving to file:
+def write_to_excel(tree, filename):
+    """Writes data from the Treeview to an Excel file."""
+
+    # Get the data from the Treeview
+    data = []
+    for item in tree.get_children():
+        values = tree.item(item, "values")
+        data.append(values)
+
+    # Create a Pandas DataFrame from the data
+    df = pd.DataFrame(data, columns=[tree.heading(col, "text") for col in tree["columns"]])
+
+    # Save the DataFrame to an Excel file
+    df.to_excel(filename.name, index=False)
+
+def file_save_as():
+    files = [('Excel File', '*.xlsx')]   
+            # Not gonna allow saving as CSV since we want it in the fomat of excel anyway.
+            #,('CSV File', '*.csv')
+    file = asksaveasfile(filetypes = files, defaultextension = files) 
+    #with open(file, "wb") as f:
+    write_to_excel(treev, file)
+
+
+#Adding a menu Bar:
+
+def donothing():
+    return
+
+menubar = Menu(r)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="New", command=donothing)
+filemenu.add_command(label="Open", command=load_excel_file)
+filemenu.add_command(label="Save", command=file_save_as)
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=r.quit)
+menubar.add_cascade(label="File", menu=filemenu)
+
+helpmenu = Menu(menubar, tearoff=0)
+helpmenu.add_command(label="Help Index", command=donothing)
+helpmenu.add_command(label="About...", command=donothing)
+menubar.add_cascade(label="Help", menu=helpmenu)
+
+r.config(menu=menubar)
 
 
 '''
 Widgets are added here
 '''
+
 r.mainloop()
